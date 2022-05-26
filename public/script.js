8581668e1874617ed7639ef7921694ced340d2f2
Sunny Bhadana <expertabhay44@gmail.com>
2022-05-26 09:37:47 +0530

    added tiny slider and adComponent

--- public/script.js
+++ public/script.js
@@ -1,45 +1,4 @@
 
-const slider = localStorage.getItem('slider')
-  
-
-$(document).ready(function() {
-
-    // slider.forEach(song => {
-    //     $('.owl-carousel').append(`
-    //     <div class="item">
-    //       <img src=${song.poster} class='w-full'/>
-    //     </div>`);
-    //   })
-
-    // $('.owl-carousel').owlCarousel({
-    //     loop:true,
-    //     margin:10,
-    //     responsive:{
-    //         0:{
-    //             items:1
-    //         },
-    //         600:{
-    //             items:1
-    //         },
-    //         1000:{
-    //             items:1
-    //         }
-    //     }
-    // })
-
-    $('.owl-carousel').owlCarousel({
-        center: true,
-        items:2,
-        loop:true,
-        margin:10,
-        responsive:{
-            600:{
-                items:2
-            }
-        }
-    });
-    
-});
   
   
   
