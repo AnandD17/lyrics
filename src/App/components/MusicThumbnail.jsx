db5a4888b6313a84922eedccf246c59027cb1973
AnandD17 <avdodamani123@gmail.com>
2022-06-04 23:16:18 +0530

    lyrics page completed✅

--- src/App/components/MusicThumbnail.jsx
+++ src/App/components/MusicThumbnail.jsx
@@ -4,12 +4,14 @@ import music from './../assets/lyrics.png'
 export const MusicThumbnail = (props) => {
   return (
     <NavLink to={`/lyrics/${props.slug}`} className='MusicThumbnail dark:bg-[#2C2C2C] w-full 2xl:h-[380px] xl:h-[317px] sm:h-[300px] h-[255px] w-full lg:min-w-[140px] m-auto rounded-[5px] bg-[white] cursor-pointer' >
+      <div className='MusicThumbnail dark:bg-[#2C2C2C] w-full 2xl:h-[380px] xl:h-[317px] sm:h-[300px] h-[255px] w-full lg:min-w-[140px] m-auto rounded-[5px] bg-[white] cursor-pointer' >
         <img src={props.img} alt="cant load image" className='aspect-square w-full rounded-t-[5px]' />
         <div className='w-[75%] bg-[#F6F6F6] h-[3px] m-auto my-5 mt-7'></div>
         <div className='text-sm text-left pl-4 pr-4 text-ellipsis dark:text-[#FAF9F6]'>
             <div className='truncate'>{props.name}</div>
             <div className='opacity-75 font-light truncate'>{props.artist}</div>
         </div>
+      </div>
     </NavLink>
   )
 }
--- src/App/pages/Lyrics.jsx
+++ src/App/pages/Lyrics.jsx
@@ -8,6 +8,7 @@ import { BASE_URL } from '../utils/apiConstant'
 import LoadingBar from "react-top-loading-bar";
 import { useLocation } from 'react-router-dom'
 import { Helmet } from 'react-helmet';
+import { MusicThumbnail } from '../components/MusicThumbnail'
 
 
 export const Lyrics = (props) => {
@@ -16,9 +17,9 @@ export const Lyrics = (props) => {
     const [tabs, setTabs] = useState('crt');
     const [progress, setProgress] = useState(0);
     const [opacity, setOpacity] = useState('');
-    const [lyricsName, setLyricsName] = useState('juice-wrld-hide-by-juice-wrld-seezyn')
     const location = useLocation();
     const [display, setDisplay] = useState(false);
+    const [additional, setAdditional] = useState();
 
 
 
@@ -36,12 +37,14 @@ export const Lyrics = (props) => {
             setDisplay('')
             setInfo(data.data.data[0]);
             setTabs(info.lyrics_crt_file !== '' ? 'crt' : info.lyrics !== '' ? 'text' : 'NA')
+            setAdditional(data.data.additional);
+            console.log(data.data.additional.by_album);
         }
 
     }
     useEffect(() => {
         getData();
-    }, [])
+    }, [location.pathname])
     return (
         <div className={`Lyrics h-full w-full overflow-hidden ${opacity} dark:bg-[#2C2C2C]`}>
 
@@ -62,7 +65,7 @@ export const Lyrics = (props) => {
 
             <div className="lg:h-[10%] sm:h-[30%] h-[23%]">
 
-            <SearchBar toggleSideBar={() => { props.toggleSideBar() }} />
+                <SearchBar toggleSideBar={() => { props.toggleSideBar() }} />
             </div>
 
             <div className={`overflow-auto lg:h-[90%] sm:h-[70%] h-[77%] bg-[#FBFBFB] px-6 pt-5 dark:bg-[#2C2C2C]  ${display}`}>
@@ -121,6 +124,52 @@ export const Lyrics = (props) => {
 
                     </div>
                 </div>
+                {additional?
+                <div className="w-full">
+                {additional.by_author.length!==0 && additional.by_author?<>
+                    <div className='flex justify-start my-4'>
+                        <ButtonHeader title={'Same Artist'} />
+                    </div>
+                    <div className='h-auto w-full  overflow-auto'>
+                        <div className="grid gap-3  gap-y-[20px] grid-cols-2 sm:grid-cols-5 sm:w-full relative overflow-auto mb-5 py-3">
+                            {additional.by_author.map((song) => {return(
+                                <div className="col-span-1 w-full" key={song.id}>
+                                    <MusicThumbnail name={song.title} artist={song.artist} img={song.poster ? song.poster : music} slug={song.slug} {...props} />
+                                </div>
+                            )})}
+                        </div>
+                    </div>
+                    </>:null}
+                    {additional.by_album.length!==0 && additional.by_album?<>
+                    <div className='flex justify-start my-4'>
+                        <ButtonHeader title={'Same Albums'} />
+                    </div>
+                    <div className='h-auto w-full  overflow-auto'>
+                        <div className="grid gap-3  gap-y-[20px] grid-cols-2 sm:grid-cols-5 sm:w-full relative overflow-auto mb-5 py-3">
+                            {additional.by_album.map((song) => { return(
+                                <div className="col-span-1 w-full" key={song.id}>
+                                    <MusicThumbnail name={song.title} artist={song.artist} img={song.poster ? song.poster : music} slug={song.slug} {...props} />
+                                </div>
+                            )})}
+                        </div>
+                    </div></>:null}
+                    {additional.by_random.length!==0 && additional.by_random?<>
+                    <div className='flex justify-start my-4'>
+                        <ButtonHeader title={'Related'} />
+                    </div>
+                    <div className='h-auto w-full  overflow-auto'>
+                        <div className="grid gap-3  gap-y-[20px] grid-cols-2 sm:grid-cols-5 sm:w-full relative overflow-auto mb-5 py-3">
+                            {additional.by_random.map((song) => {return(
+                                <div className="col-span-1 w-full" key={song.id}>
+                                    <MusicThumbnail name={song.title} artist={song.artist} img={song.poster ? song.poster : music} slug={song.slug} {...props} />
+                                </div>
+                            )})}
+                        </div>
+                    </div>
+                    </>:null}
+                    </div>
+                :null}
+                
             </div>
         </div>
     )
